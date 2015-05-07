94b4648874545133536fbc5f5b6b4c9fcbfa27d8
jayfo <jayfo.github@gmail.com>
2015-05-07 15:13:17 -0700

    Detect Change of Repository

--- run.sh
+++ run.sh
@@ -6,6 +6,17 @@ if [[ ! -n $JEKYLL_GIT_REPOSITORY ]] ; then
   exit 1
 fi
 
+# If we have an existing repository, but not the right one, we need to start from scratch
+EXISTING_GIT_REPOSITORY=$(git --git-dir=/site/.git remote -v | grep -m 1 origin | awk -F'[ \t]' '{print $2}')
+echo "Desired Repository: $JEKYLL_GIT_REPOSITORY"
+echo "Existing Repository: $EXISTING_GIT_REPOSITORY"
+if [[ -d /site ]] ; then
+  if [[ $JEKYLL_GIT_REPOSITORY != $EXISTING_GIT_REPOSITORY ]] ; then
+    rm -rf /site/*
+    rm -rf /site/.[!.]?*
+  fi
+fi
+
 # Check whether this is our first time running, such that we need to clone
 if [[ ! -d /site/.git ]] ; then
   git clone $JEKYLL_GIT_REPOSITORY /site
